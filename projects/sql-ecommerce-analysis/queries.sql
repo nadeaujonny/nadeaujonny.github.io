c7c2866f2839241319bb5327c4c93b29143d8e1f
Jonathan Nadeau <44739067+nadeaujonny@users.noreply.github.com>
2026-01-23 20:46:12 -0800

    Update queries.sql

--- projects/sql-ecommerce-analysis/queries.sql
+++ projects/sql-ecommerce-analysis/queries.sql
@@ -15,6 +15,7 @@
    - filtering
    - grouping
    - ordering
+   - SUM aggregations
 ========================================================= */
 
 SELECT 
@@ -36,6 +37,8 @@ GROUP BY product_name, product_category
 
 ORDER BY total_revenue DESC
 LIMIT 10;
+
+---
 
 /* =========================================================
    BUSINESS QUESTION:
@@ -69,7 +72,53 @@ WITH product_profit AS (
   GROUP BY p.id, p.name
 )
 
+   
 SELECT *
 FROM product_profit
 ORDER BY profit DESC
+LIMIT 10;
+
+---
+
+/* =========================================================
+   BUSINESS QUESTION:
+   What are the top 10 brands by profit?
+
+   DATASET:
+   bigquery-public-data.thelook_ecommerce
+
+   METRICS:
+   - revenue
+   - profit
+   - units_sold
+
+   SQL FEATURES:
+   - CTEs
+   - window functions
+========================================================= */
+WITH brand_metrics AS (
+  SELECT
+    p.brand,
+    ROUND(SUM(oi.sale_price), 2) AS revenue,
+    ROUND(SUM(oi.sale_price - p.cost), 2) AS profit,
+    COUNT(*) AS units_sold
+  FROM `bigquery-public-data.thelook_ecommerce.order_items` oi
+  JOIN `bigquery-public-data.thelook_ecommerce.orders` o
+    ON oi.order_id = o.order_id
+  JOIN `bigquery-public-data.thelook_ecommerce.products` p
+    ON oi.product_id = p.id
+  WHERE o.status = 'Complete'
+  GROUP BY p.brand
+),
+
+ranked_brands AS (
+  SELECT *,
+         RANK() OVER (ORDER BY profit DESC) AS profit_rank,
+         RANK() OVER (ORDER BY revenue DESC) AS revenue_rank
+  FROM brand_metrics
+)
+
+SELECT *
+FROM ranked_brands
+ORDER BY profit_rank
 LIMIT 10;
