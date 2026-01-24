8d18c3656bf6af6a3456b181100755ec0a38f95f
Jonathan Nadeau <44739067+nadeaujonny@users.noreply.github.com>
2026-01-23 20:24:50 -0800

    Update queries.sql

--- projects/sql-ecommerce-analysis/queries.sql
+++ projects/sql-ecommerce-analysis/queries.sql
@@ -9,6 +9,12 @@
    - total_revenue
    - total_profit
    - units_sold
+
+   SQL FEATURES:
+   - joins
+   - filtering
+   - grouping
+   - ordering
 ========================================================= */
 
 SELECT 
@@ -31,3 +37,39 @@ GROUP BY product_name, product_category
 ORDER BY total_revenue DESC
 LIMIT 10;
 
+/* =========================================================
+   BUSINESS QUESTION:
+   What are the top 10 products by total profit?
+
+   DATASET:
+   bigquery-public-data.thelook_ecommerce
+
+   METRICS:
+   - total_revenue
+   - total_profit
+   - units_sold
+
+   SQL FEATURES:
+   - CTE
+========================================================= */
+
+WITH product_profit AS (
+  SELECT
+    p.id AS product_id,
+    p.name AS product_name,
+    ROUND(SUM(oi.sale_price), 2) AS revenue,
+    ROUND(SUM(oi.sale_price - p.cost), 2) AS profit,
+    COUNT(*) AS units_sold
+  FROM `bigquery-public-data.thelook_ecommerce.order_items` oi
+  JOIN `bigquery-public-data.thelook_ecommerce.orders` o
+    ON oi.order_id = o.order_id
+  JOIN `bigquery-public-data.thelook_ecommerce.products` p
+    ON oi.product_id = p.id
+  WHERE o.status = 'Complete'
+  GROUP BY p.id, p.name
+)
+
+SELECT *
+FROM product_profit
+ORDER BY profit DESC
+LIMIT 10;
